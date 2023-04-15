2a2f5ae5f9630a2b4f901a92ffb3f20be31bc456
Leah Rowe <leah@libreboot.org>
2023-04-15 15:48:11 +0100

    general code cleanup (comments and declarations)
    i forgot to put a line break for curly brackets
    in functions. fixed now.
    
    also use consistent style for comments
    
    squash comments, and code lines

--- e6400_flash_unlock.c
+++ e6400_flash_unlock.c
@@ -50,8 +50,7 @@ int set_gbl_smi_en(int enable);
 #define PMBASE 0x1000
 #define SMI_EN_REG (PMBASE + 0x30)
 
-// Hardcoded to what E6400 vendor BIOS sets it to
-// Assuming this doesn't change between versions
+/* Assume this is the same on all vendor BIOS versions */
 #define RCBA 0xfed18000
 
 #define RCBA_MMIO_LEN 0x4000
@@ -61,48 +60,34 @@ int set_gbl_smi_en(int enable);
 volatile uint8_t *rcba_mmio;
 
 int
-main(int argc, char *argv[]) {
+main(int argc, char *argv[])
+{
 	int devmemfd;
-
 	(void)argc;
 	(void)argv;
 
 	if ((ioperm(EC_INDEX, 2, 1) == -1) || (ioperm(SMI_EN_REG, 4, 1) == -1))
 		err(errno, "Could not access IO ports");
-
 	if ((devmemfd = open("/dev/mem", O_RDONLY)) == -1)
 		err(errno, "/dev/mem");
 
-	// Flash Descriptor Override Pin-Strap Status bit is in RCBA mmio space
+	/* FDO pin-strap status bit is in RCBA mmio space */
 	rcba_mmio = mmap(0, RCBA_MMIO_LEN, PROT_READ, MAP_SHARED, devmemfd,
 			RCBA);
 	if (rcba_mmio == MAP_FAILED)
 		err(errno, "Could not map RCBA");
 
-	if (get_fdo_status() == 1) {
-		// FDO strap not set, so tell EC to set it on next boot
+	if (get_fdo_status() == 1) { /* tell EC to set FDO straps upon boot */
 		ec_fdo_command(2);
-		printf("Flash Descriptor override enabled. Please *shutdown* "
-			"(don't reboot) the system.\nThe EC will automatically"
-			"boot the system and set the descriptor override.\n"
-			"Then run this utility again to complete the unlock"
-			"process\n");
-	} else if (get_gbl_smi_en()){
-		/*
-		 * SMIs enabled, so disable them to bypass
-		 * the BIOS Lock function
-		 */
+		printf("Flash Descriptor Override enabled. Shut down now. The "
+			"EC will auto-boot the system and set the override.\n"
+			"Upon boot, re-run this utility to unlock flash.\n");
+	} else if (get_gbl_smi_en()){ /* disable SMIs for internal flashing */
 		set_gbl_smi_en(0);
-		printf("SMIs disabled, you should now be able to run flashrom "
-			"-p internal on the entire flash!\n\nAfter you are "
-			"done flashing, run this utility again to re-enable "
-			"SMIs,\n as the system will not power off properly if "
-			"SMIs are disabled.\n");
-	} else {
-		/*
-		 * SMIs disabled, so re-enable them so that
-		 * shutdown works properly
-		 */
+		printf("SMIs disabled. Internal flashing should work now.\n"
+			"After flashing, re-run this utility to enable SMIs.\n"
+			"(shutdown is buggy when SMIs are disabled)\n");
+	} else { /* enable SMIs so that the system can shut down properly */
 		set_gbl_smi_en(1);
 		printf("SMIs enabled, you can now shutdown the system.\n");
 	}
@@ -110,38 +95,43 @@ main(int argc, char *argv[]) {
 }
 
 int
-get_fdo_status(void) {
+get_fdo_status(void)
+{
 	return (*(uint16_t*)(rcba_mmio + SPIBAR + HSFS_REG) >> 13) & 1;
 }
 
-/**
+/*
  * arg:
  * 0 = Query EC FDO status - TODO
  * 2 = Enable FDO for next boot
  * 3 = Disable FDO for next boot - TODO
  */
 uint8_t
-ec_fdo_command(enum EC_FDO_CMD arg) {
+ec_fdo_command(enum EC_FDO_CMD arg)
+{
 	write_ec_reg(0x12, arg);
 	send_ec_cmd(0xb8);
 	return 1;
 }
 
 void
-write_ec_reg(uint8_t index, uint8_t data) {
+write_ec_reg(uint8_t index, uint8_t data)
+{
 	outb(index, EC_INDEX);
 	outb(data, EC_DATA);
 }
 
 void
-send_ec_cmd(uint8_t cmd) {
+send_ec_cmd(uint8_t cmd)
+{
 	outb(0, EC_INDEX);
 	outb(cmd, EC_DATA);
 	wait_ec();
 }
 
 void
-wait_ec(void) {
+wait_ec(void)
+{
 	uint8_t busy;
 	do {
 		outb(0, EC_INDEX);
@@ -150,12 +140,14 @@ wait_ec(void) {
 }
 
 int
-get_gbl_smi_en(void) {
+get_gbl_smi_en(void)
+{
 	return inl(SMI_EN_REG) & 1;
 }
 
 int
-set_gbl_smi_en(int enable) {
+set_gbl_smi_en(int enable)
+{
 	uint32_t smi_en = inl(SMI_EN_REG);	
 	if (enable) {
 		smi_en |= 1;
@@ -168,7 +160,8 @@ set_gbl_smi_en(int enable) {
 
 /*
 uint8_t
-read_ec_reg(uint8_t index) {
+read_ec_reg(uint8_t index)
+{
 	outb(index, EC_INDEX);
 	return inb(EC_DATA);
 }
